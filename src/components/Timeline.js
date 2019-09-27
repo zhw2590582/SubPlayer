50a12b32a9ac20da494bdc5802eb80d0afc3b507
Harvey <laozhaochaguan@gmail.com>
2019-09-27 17:47:21 +0800

    绘制标尺

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { secondToTime } from '../utils';
 
 const Wrapper = styled.div`
     display: flex;
@@ -8,16 +9,30 @@ const Wrapper = styled.div`
     background-color: rgb(28, 32, 34);
 `;
 
-function drawGrid(ctx, width) {
-    const num = 100;
+function drawGrid(ctx, width, beginTime = 0) {
+    let timeIndex = 0;
+    const num = 110;
     const height = 200;
     const pre = width / num;
-    ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+    ctx.font = '12px';
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     for (let index = 0; index < num; index++) {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
         ctx.fillRect(pre * index, 0, 1, height);
+        if (index % 10 === 0) {
+            ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+            ctx.fillRect(pre * index, 0, 1, 7);
+        } else if (index % 5 === 0) {
+            ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
+            ctx.fillRect(pre * index, 0, 1, 15);
+            ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+            ctx.fillText(secondToTime(beginTime + timeIndex++).split('.')[0], pre * index - 20, 30);
+        }
     }
 
     for (let index = 0; index < height / pre; index++) {
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
         ctx.fillRect(0, pre * index, width, 1);
     }
 }
@@ -33,7 +48,6 @@ export default class Timeline extends React.Component {
             if (props.mainWidth !== $canvas.width) {
                 $canvas.width = props.mainWidth;
                 const ctx = $canvas.getContext('2d');
-                ctx.clearRect(0, 0, $canvas.width, $canvas.height);
                 drawGrid(ctx, $canvas.width);
             }
         }
