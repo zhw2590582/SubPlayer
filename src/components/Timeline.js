b07f8476d41ca5f534ee69ec1a3ef9d7d651bdb3
Harvey <laozhaochaguan@gmail.com>
2019-09-27 17:12:07 +0800

    绘制网格

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,21 +8,42 @@ const Wrapper = styled.div`
     background-color: rgb(28, 32, 34);
 `;
 
-function drawbackground($canvas) {
-    console.log($canvas);
+function drawGrid(ctx, width) {
+    const num = 100;
+    const height = 200;
+    const pre = width / num;
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
+    for (let index = 0; index < num; index++) {
+        ctx.fillRect(pre * index, 0, 1, height);
+    }
+
+    for (let index = 0; index < height / pre; index++) {
+        ctx.fillRect(0, pre * index, width, 1);
+    }
 }
 
 export default class Timeline extends React.Component {
-    $canvas = React.createRef();
+    state = {
+        $canvas: React.createRef(),
+    };
 
-    componentDidMount() {
-        drawbackground(this.$canvas.current);
+    static getDerivedStateFromProps(props, state) {
+        if (state.$canvas.current) {
+            const $canvas = state.$canvas.current;
+            if (props.mainWidth !== $canvas.width) {
+                $canvas.width = props.mainWidth;
+                const ctx = $canvas.getContext('2d');
+                ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+                drawGrid(ctx, $canvas.width);
+            }
+        }
+        return null;
     }
 
     render() {
         return (
             <Wrapper>
-                <canvas width={this.props.mainWidth} height={200} ref={this.$canvas} />
+                <canvas width="200" height="200" ref={this.state.$canvas} />
             </Wrapper>
         );
     }
