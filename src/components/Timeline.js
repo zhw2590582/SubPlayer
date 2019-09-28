57e8bb912f93e142ba6cd4594a56ef1c61e1be44
zhw2590582 <laozhaochaguan@gmail.com>
2019-09-28 08:17:23 +0800

    添加坐标线

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,77 +2,110 @@ import React from 'react';
 import styled from 'styled-components';
 import { secondToTime } from '../utils';
 
+const timelineHeight = 200;
 const Wrapper = styled.div`
     position: relative
     display: flex;
-    height: 200px;
+    height: ${timelineHeight}px;
     background-color: rgb(28, 32, 34);
 
     canvas {
         position: absolute;
-        z-index: 5;
+        z-index: 2;
         left: 0;
         top: 0;
         pointer-events: none;
+        transition: all 0.2s ease;
     }
 `;
 
-function drawGrid(ctx, width, beginTime = 0) {
-    let timeIndex = 0;
-    const num = 110;
-    const height = 200;
-    const pre = width / num;
-    ctx.font = '22px Arial';
+const Line = styled.div`
+    position: absolute;
+    z-index: 3;
+    left: 0;
+    top: 0;
+    width: 1px;
+    height: 100%;
+    transition: all 0.2s ease;
+    background-color: #f44336;
+`;
 
+function drawGrid(ctx, width, beginTime = 0) {
+    let ruler = 0;
+    const num = 110;
+    const height = timelineHeight;
+    const gap = width / num;
+
+    ctx.font = '22px Arial';
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.05)';
+    ctx.fillRect(0, 0, gap * 5, height * 2);
+    ctx.fillRect(105 * gap, 0, gap * 5, height * 2);
     for (let index = 0; index < num; index++) {
         ctx.fillStyle = 'rgba(255, 255, 255, 0.05)';
-        ctx.fillRect(pre * index, 0, 2, height * 2);
+        ctx.fillRect(gap * index, 0, 2, height * 2);
         if (index % 10 === 0) {
             ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
             if (index) {
-                ctx.fillRect(pre * index, 0, 2, 15);
+                ctx.fillRect(gap * index, 0, 2, 15);
             }
         } else if (index % 5 === 0) {
             ctx.fillStyle = 'rgba(255, 255, 255, 0.5)';
-            ctx.fillRect(pre * index, 0, 2, 30);
+            ctx.fillRect(gap * index, 0, 2, 30);
             ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-            ctx.fillText(secondToTime(beginTime + timeIndex++).split('.')[0], pre * index - 42, 60);
+            ctx.fillText(secondToTime(beginTime + ruler++).split('.')[0], gap * index - 42, 60);
         }
     }
 
-    for (let index = 0; index < (height / pre) * 2; index++) {
+    for (let index = 0; index < (height / gap) * 2; index++) {
         ctx.fillStyle = 'rgba(255, 255, 255, 0.05)';
-        ctx.fillRect(0, pre * index, width, 2);
+        ctx.fillRect(0, gap * index, width, 2);
     }
 
-    return pre;
+    const grid = Number((gap / 2).toFixed(3));
+    const padding = grid * 5;
+    return {
+        grid,
+        padding,
+    };
 }
 
 export default class Timeline extends React.Component {
     state = {
         $canvas: React.createRef(),
+        grid: 0,
+        padding: 0,
     };
 
     static getDerivedStateFromProps(props, state) {
         if (state.$canvas.current) {
             const $canvas = state.$canvas.current;
             if (props.mainWidth !== $canvas.width) {
-                $canvas.height = 200 * 2;
+                $canvas.height = timelineHeight * 2;
                 $canvas.width = props.mainWidth * 2;
-                $canvas.style.height = '200px';
+                $canvas.style.height = '100%';
                 $canvas.style.width = `${props.mainWidth}px`;
                 const ctx = $canvas.getContext('2d');
-                drawGrid(ctx, $canvas.width);
+                const { grid, padding } = drawGrid(ctx, $canvas.width);
+                return {
+                    grid,
+                    padding,
+                };
             }
         }
         return null;
     }
 
     render() {
+        const { $canvas, grid, padding } = this.state;
         return (
             <Wrapper>
-                <canvas ref={this.state.$canvas} />
+                <canvas ref={$canvas} />
+                <Line
+                    style={{
+                        transform: `translate(${padding + grid}px)`,
+                    }}
+                ></Line>
             </Wrapper>
         );
     }
