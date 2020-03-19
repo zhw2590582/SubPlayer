0fbdeb20e4b9e5ea51fff51a2b3c24ec860a4cf6
Harvey <laozhaochaguan@gmail.com>
2020-03-19 18:47:10 +0800

    add exportImage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,33 +10,42 @@ const Footer = styled.div`
     .timeline-header {
         display: flex;
         align-items: center;
+        justify-content: space-between;
         height: 35px;
         padding: 0 10px;
         font-size: 12px;
         border-bottom: 1px solid #000;
+        color: rgba(255, 255, 255, 0.5);
         background-color: rgba(0, 0, 0, 0.3);
 
-        .item {
+        .timeline-header-left {
             display: flex;
-            margin-right: 20px;
-
-            .name {
-                color: rgba(255, 255, 255, 0.5);
-                margin-right: 10px;
-            }
-
-            .value {
+            align-items: center;
+            .item {
                 display: flex;
-                align-items: center;
+                margin-right: 20px;
 
-                input {
-                    height: 3px;
-                    width: 100px;
-                    outline: none;
-                    appearance: none;
-                    background-color: rgba(255, 255, 255, 0.2);
+                .name {
+                    margin-right: 10px;
+                }
+
+                .value {
+                    display: flex;
+                    align-items: center;
+
+                    input {
+                        height: 3px;
+                        width: 100px;
+                        outline: none;
+                        appearance: none;
+                        background-color: rgba(255, 255, 255, 0.2);
+                    }
                 }
             }
+        }
+
+        .timeline-header-right {
+            cursor: pointer;
         }
     }
 
@@ -82,46 +91,57 @@ export default function(props) {
     return (
         <Footer>
             <div className="timeline-header">
-                <div className="item">
-                    <div className="name">Decoding Progress:</div>
-                    <div className="value">0%</div>
-                </div>
-                <div className="item">
-                    <div className="name">Unit Duration:</div>
-                    <div className="value">
-                        <input
-                            defaultValue="10"
-                            type="range"
-                            min="5"
-                            max="20"
-                            step="1"
-                            onChange={event => {
-                                if (!wf) return;
-                                wf.setOptions({
-                                    duration: Number(event.target.value || 10),
-                                });
-                            }}
-                        />
+                <div className="timeline-header-left">
+                    <div className="item">
+                        <div className="name">Decoding Progress:</div>
+                        <div className="value">0%</div>
+                    </div>
+                    <div className="item">
+                        <div className="name">Unit Duration:</div>
+                        <div className="value">
+                            <input
+                                defaultValue="10"
+                                type="range"
+                                min="5"
+                                max="20"
+                                step="1"
+                                onChange={event => {
+                                    if (!wf) return;
+                                    wf.setOptions({
+                                        duration: Number(event.target.value || 10),
+                                    });
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="name">Height Zoom:</div>
+                        <div className="value">
+                            <input
+                                defaultValue="1"
+                                type="range"
+                                min="0.1"
+                                max="2"
+                                step="0.1"
+                                disabled={!props.options.useAudioWaveform}
+                                onChange={event => {
+                                    if (!wf) return;
+                                    wf.setOptions({
+                                        waveScale: Number(event.target.value || 1),
+                                    });
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
-                <div className="item">
-                    <div className="name">Height Zoom:</div>
-                    <div className="value">
-                        <input
-                            defaultValue="1"
-                            type="range"
-                            min="0.1"
-                            max="2"
-                            step="0.1"
-                            disabled={!props.options.useAudioWaveform}
-                            onChange={event => {
-                                if (!wf) return;
-                                wf.setOptions({
-                                    waveScale: Number(event.target.value || 1),
-                                });
-                            }}
-                        />
-                    </div>
+                <div
+                    className="timeline-header-right"
+                    onClick={() => {
+                        if (!wf) return;
+                        wf.exportImage();
+                    }}
+                >
+                    Export Waveform Image
                 </div>
             </div>
             <div className="timeline-body">{props.player ? <Waveform {...props} /> : null}</div>
