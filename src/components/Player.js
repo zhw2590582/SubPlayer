dcf85e347ce876b2eceaf4bb7ba6a98fe805365e
Harvey <laozhaochaguan@gmail.com>
2020-03-21 17:14:20 +0800

    change video

--- src/components/Player.js
+++ src/components/Player.js
@@ -35,6 +35,7 @@ export default React.memo(
                         },
                         moreVideoAttr: {
                             crossOrigin: 'anonymous',
+                            preload: 'auto',
                         },
                     }}
                     getInstance={art => {
