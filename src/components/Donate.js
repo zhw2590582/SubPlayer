199e15e0b70ca2c64d437110e2aff5ebe8e5160f
Harvey <laozhaochaguan@gmail.com>
2020-03-21 13:41:38 +0800

    add img

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -3,8 +3,38 @@ import styled from 'styled-components';
 
 const Donate = styled.div`
     position: relative;
+    padding: 0 20px 20px;
+    font-size: 14px;
+
+    a {
+        color: #2196f3;
+    }
 `;
 
 export default function() {
-    return <Donate>Donate</Donate>;
+    return (
+        <Donate>
+            <p>
+                Or you can buy me a cup of coffee to encourage me to continue to update and improve the subtitle editor.
+            </p>
+            <p>Paypal:</p>
+            <p>
+                <a href="https://www.paypal.me/harveyzack">https://www.paypal.me/harveyzack</a>
+            </p>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div>
+                    <p>WeChat Pay:</p>
+                    <p>
+                        <img style={{ width: 210 }} src="/wechatpay.jpg" alt="WeChat Pay" />
+                    </p>
+                </div>
+                <div>
+                    <p>Alipay:</p>
+                    <p>
+                        <img style={{ width: 210 }} src="/alipay.jpg" alt="Alipay" />
+                    </p>
+                </div>
+            </div>
+        </Donate>
+    );
 }
--- src/components/Help.js
+++ src/components/Help.js
@@ -3,8 +3,31 @@ import styled from 'styled-components';
 
 const Help = styled.div`
     position: relative;
+    padding: 0 20px 20px;
+    font-size: 14px;
+
+    a {
+        color: #2196f3;
+    }
 `;
 
 export default function() {
-    return <Help>Help</Help>;
+    return (
+        <Help>
+            <p>
+                This editor is suitable for video with small volume and simple subtitle effect. For large file videos or
+                more subtitle effect, please use professional desktop software.
+            </p>
+            <p>You can ask any questions on Github Issue:</p>
+            <p>
+                <a href="https://github.com/zhw2590582/SubPlayer/issues">
+                    https://github.com/zhw2590582/SubPlayer/issues
+                </a>
+            </p>
+            <p>Or contact me via email:</p>
+            <p>
+                <a href="mailto:laozhaochaguan@gmail.com">laozhaochaguan@gmail.com</a>
+            </p>
+        </Help>
+    );
 }
