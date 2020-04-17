7c0f6a702e4f0757b647ac900a39c26cc3b388e8
Ihor Bohdanov <bbengamin@gmail.com>
2020-04-17 18:40:25 +0300

    Add flow-player to main page with x1,x5,x10 speed options

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,90 +1,104 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactFlowPlayer from "react-flow-player";
 import ArtplayerComponent from 'artplayer-react';
 
 const Player = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
-    height: 70%;
+    justify-content: space-between;
+    height: 75%;
     width: 100%;
     padding: 20px;
     background-color: rgba(0, 0, 0, 0.5);
+    & > div {
+        width: 49%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 `;
+let fPlayer = null
+let aPlayer = null
 
+function flowPlayerActive() {
+  aPlayer.pause = true
+  fPlayer.seekTo(aPlayer.currentTime / 10)
+}
+function artPlayerActive() {
+  fPlayer.stop()
+  aPlayer.seek = fPlayer.video.time
+}
 export default React.memo(
+
+
     function({ options, setPlayer, setCurrentTime }) {
         return (
             <Player>
-                <ArtplayerComponent
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                    }}
-                    option={{
-                        url: options.videoUrl,
-                        loop: true,
-                        autoSize: true,
-                        aspectRatio: true,
-                        playbackRate: true,
-                        fullscreen: true,
-                        fullscreenWeb: true,
-                        miniProgressBar: true,
-                        subtitle: {
-                            url: options.subtitleUrl,
-                        },
-                        moreVideoAttr: {
-                            crossOrigin: 'anonymous',
-                            preload: 'auto',
-                        },
-                    }}
-                    getInstance={art => {
-                        setPlayer(art);
+              <ArtplayerComponent
+                style={{
+                  width: '49%',
+                  height: '100%',
+                }}
+                option={{
+                  url: options.videoUrl,
+                  loop: true,
+                  autoSize: true,
+                  aspectRatio: true,
+                  playbackRate: true,
+                  fullscreen: true,
+                  fullscreenWeb: true,
+                  miniProgressBar: true,
+                  subtitle: {
+                    url: options.subtitleUrl,
+                  },
+                  moreVideoAttr: {
+                    crossOrigin: 'anonymous',
+                    preload: 'auto',
+                  },
+                }}
+                getInstance={art => {
+                  setPlayer(art);
+                  aPlayer = art;
 
-                        (function loop() {
-                            window.requestAnimationFrame(() => {
-                                if (art.playing) {
-                                    setCurrentTime(art.currentTime);
-                                }
-                                loop();
-                            });
-                        })();
+                  (function loop() {
+                    window.requestAnimationFrame(() => {
+                      if (art.playing) {
+                        setCurrentTime(art.currentTime);
+                      }
+                      loop();
+                    });
+                  })();
 
-                        art.on('seek', () => {
-                            setCurrentTime(art.currentTime);
-                        });
+                  art.on('seek', () => {
+                    setCurrentTime(art.currentTime);
+                  });
 
-                        art.option.controls = [
-                            {
-                                position: 'right',
-                                html: '0.5X',
-                                click: function() {
-                                    art.playbackRate = 0.5;
-                                },
-                            },
-                            {
-                                position: 'right',
-                                html: '1X',
-                                click: function() {
-                                    art.playbackRate = 1;
-                                },
-                            },
-                            {
-                                position: 'right',
-                                html: '1.5X',
-                                click: function() {
-                                    art.playbackRate = 1.5;
-                                },
-                            },
-                            {
-                                position: 'right',
-                                html: '2X',
-                                click: function() {
-                                    art.playbackRate = 2;
-                                },
-                            }
-                        ]
-                    }}
+                  art.on('play', () => {
+                    artPlayerActive()
+                  });
+                }}
+              />
+                <ReactFlowPlayer
+                  playerInitScript="http://releases.flowplayer.org/7.2.1/flowplayer.min.js"
+                  playerId="reactFlowPlayer"
+                  sources={[
+                      {
+                          type: "video/webm",
+                          src: options.videoUrl
+                      }
+                  ]}
+                  speedPlugin={ true }
+                  speeds={[1, 5, 10]}
+                  onResume={() => { flowPlayerActive() }}
+                  getPlayerApi={player => {
+                    fPlayer = player
+
+                    fPlayer.on('pause', () => {
+                      artPlayerActive()
+                    });
+                  }}
                 />
             </Player>
         );
