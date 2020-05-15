d8b0b99101fb998d6b308288e83131a6c270cc4d
Ahmad Ali <aallii300300@gmail.com>
2020-05-16 00:38:09 +0100

    video styling, loop through selected subtitles

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import ArtplayerComponent from 'artplayer-react';
+import './css/player.css';
+import { timeToDuration, durationToTime }  from '../waveform/utils';
 
 const Player = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 70%;
+    height: 100%;
     width: 100%;
-    padding: 20px;
-    background-color: rgba(0, 0, 0, 0.5);
+    padding: 5px;
+    background-color: rgba(0, 0, 0, 0.2);
 `;
 
 export default React.memo(
@@ -32,6 +34,11 @@ export default React.memo(
                         miniProgressBar: true,
                         subtitle: {
                             url: options.subtitleUrl,
+                            style: {
+                                color: '#f9fed6',
+                                backgroundColor : 'rgba(0, 0, 0, 0.4)',
+                                fontSize: '1.5rem'
+                            }
                         },
                         moreVideoAttr: {
                             crossOrigin: 'anonymous',
@@ -39,19 +46,44 @@ export default React.memo(
                         },
                     }}
                     getInstance={art => {
+                        console.log('art',art.currentTime)
                         setPlayer(art);
 
                         (function loop() {
                             window.requestAnimationFrame(() => {
+
+                                 //console.log('request frame', art.currentTime)
+                               // console.log('loop end', window.loopEnd)
+                                //console.log('lopp start', window.loopStart)
+
+                                if (window.loopEnd === null && window.loopStart === null){
                                 if (art.playing) {
                                     setCurrentTime(art.currentTime);
+                                }
+                            }
+
+                              
+                                if(window.loopEnd !== null){
+                               // console.log(timeToDuration(window.loopEnd) )
+                               // console.log(durationToTime(window.loopEnd) )
+                               // console.log(window.loopEnd) 
+                                
+                                if(art.currentTime > timeToDuration(window.loopEnd)){
+                                    art.currentTime = timeToDuration(window.loopStart);
+                                }
                                 }
                                 loop();
                             });
                         })();
 
+                       
                         art.on('seek', () => {
-                            setCurrentTime(art.currentTime);
+                            if (window.loopEnd === null && window.loopStart === null){
+                                setCurrentTime(art.currentTime);
+                            }
+                        
+                        //    console.log('loop end', window.loopEnd)
+                        //         console.log('lopp start', window.loopStart)
                         });
                     }}
                 />
