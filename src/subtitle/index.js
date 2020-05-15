303549ad7197df36dc57b2c4c654af22b1175963
Ahmad Ali <aallii300300@gmail.com>
2020-05-16 00:45:49 +0100

    export fix time

--- src/subtitle/index.js
+++ src/subtitle/index.js
@@ -200,3 +200,24 @@ export async function getSubFromVttUrl(url) {
         $track.src = url;
     });
 }
+
+export  function fixTime(time = '') {
+    return time
+        .split(/[:.]/)
+        .map((item, index, arr) => {
+            if (index === arr.length - 1) {
+                if (item.length === 1) {
+                    return '.' + item + '00';
+                } else if (item.length === 2) {
+                    return '.' + item + '0';
+                }
+            } else {
+                if (item.length === 1) {
+                    return (index === 0 ? '0' : ':0') + item;
+                }
+            }
+
+            return index === 0 ? item : index === arr.length - 1 ? '.' + item : ':' + item;
+        })
+        .join('');
+}
